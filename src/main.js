2a3c85a057e63d4acfa445ed9d7b2e588ec887dd
Студенческая <@rbiu.local>
2025-11-21 13:01:33 +0500

    firstcard

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import './style.css'
+import 'flowbite';
+
+// import Swiper JS
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+ let swiper = new Swiper(".mySwiper", {
+    modules: [Navigation, Pagination],
+      direction: "vertical",
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+    
